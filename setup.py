3a72bbb86cd53a332711d05e1fb96b45004ff73b
Ning Liu <4315932+0liu@users.noreply.github.com>
2020-03-25 15:42:52 -0700

    Reorg.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3, 7, 1):
 
 def read_version():
     regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
-    init_py = os.path.join(os.path.dirname(__file__), 'trade_china', '__init__.py')
+    init_py = os.path.join(os.path.dirname(__file__), 'trader_dash', '__init__.py')
     with open(init_py) as f:
         for line in f:
             match = regexp.match(line)
@@ -25,7 +25,7 @@ with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='trade_china',
+    name='trader_dash',
     version=read_version(),
     description=('Trading dashboard for China market.'),
     long_description=long_description,
@@ -39,12 +39,10 @@ setup(
         'Topic :: Scientific/Engineering',
     ],
     platforms=['any'],
-    author="Jesse Liu",
-    author_email="jesseliu0@gmail.com",
-    url='https://github.com/jesseliu0/trade_china',
-    download_url='https://pypi.python.org/pypi/trade_china',
+    author="Ning Liu",
+    url='https://github.com/0liu/trader_dash',
     license='Apache 2.0',
-    packages=['trade_china'],
+    packages=['trader_dash'],
     include_package_data=True,
     python_requires='>=3.7.1',
     install_requires=['pytz>=2018.9', 'pymysql>=0.9.3', 'SQLAlchemy>=1.3.1', 'pandas>=0.24.2', 'xlwings>=0.15.4', 'pika==0.13.0', 'avro-python3>=1.8.2', 'coloredlogs>=10.0'],
